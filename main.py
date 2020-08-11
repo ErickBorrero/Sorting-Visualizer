b1db84c30c02e651d31c20711469c49eed370395
ItsTheFlea <59777514+ItsTheFlea@users.noreply.github.com>
2020-08-11 00:47:16 -0400

    Revamp
    Decided to scrap using python extensions to make a GUI and visualizer. Switching to html/css. Working on website GUI

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 import time
 import random
+import tkinter as tk
+from tkinter import Text
 
 def updateFig(x, rectangles, iteration):
     for rectangle, value in zip(rectangles, x[0]):
@@ -11,7 +13,6 @@ def updateFig(x, rectangles, iteration):
     iteration[0] += 1
     text.set_text("# of operations: {}".format(iteration[0]))
     
-
 def bubbleSort(nums):
     last = len(nums)
     while last != 0:
@@ -55,24 +56,29 @@ def merge(a, b):
         a[i] = sorted_val
         yield a
 
-randomArray = [random.randint(0,50) for i in range(50)]
+def createGraph():
+    randomArray = [random.randint(0,50) for i in range(50)]
 
-title = 'Bubble Sort'
-generator = bubbleSort(randomArray)
+    title = 'Bubble Sort'
+    generator = bubbleSort(randomArray)
 
-fig, ax = plt.subplots()
-ax.set_title(title)
-rectangleBars = ax.bar(range(len(randomArray)), randomArray, align='edge')
-text = ax.text(0.02, 0.95, "", transform=ax.transAxes)
-iteration = [0]
+    fig, ax = plt.subplots()
+    ax.set_title(title)
+    rectangleBars = ax.bar(range(len(randomArray)), randomArray, align='edge')
+    text = ax.text(0.02, 0.95, "", transform=ax.transAxes)
+    iteration = [0]
 
-anim = animation.FuncAnimation(fig, 
-                               func = updateFig,
-                               fargs = (rectangleBars, iteration), 
-                               frames = generator, 
-                               interval = 1, 
-                               repeat = False
-                               )
+    anim = animation.FuncAnimation(fig, 
+                                func = updateFig,
+                                fargs = (rectangleBars, iteration), 
+                                frames = generator, 
+                                interval = 1, 
+                                repeat = False
+                                )
 
 
-plt.show()
+    plt.show()
+
+root = tk.Tk()
+
+root.mainloop()
